3e18549c25097a1779467018dde2076d9e45924d
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-08-02 21:19:36 +0545

    initial commit

---
+++ src/main/java/com/expense_tracker_app/expense_tracker_app/ExpenseTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.expense_tracker_app.expense_tracker_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerAppApplication.class, args);
+	}
+
+}
