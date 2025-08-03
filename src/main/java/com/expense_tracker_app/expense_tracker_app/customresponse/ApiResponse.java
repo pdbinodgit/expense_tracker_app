552c037b6a99ff3e6694360bf9a643c139d99236
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-08-03 20:06:43 +0545

    adding custom response class

---
+++ src/main/java/com/expense_tracker_app/expense_tracker_app/customresponse/ApiResponse.java
@@ -0,0 +1,18 @@
+package com.expense_tracker_app.expense_tracker_app.customresponse;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiResponse<T> {
+
+    private HttpStatus status;
+
+    private String message;
+
+    private T data;
+
+    public ApiResponse(HttpStatus status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+}
