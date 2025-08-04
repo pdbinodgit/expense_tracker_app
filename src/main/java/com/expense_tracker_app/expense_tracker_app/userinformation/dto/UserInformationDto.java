4384a2c931bd3de36243752db91c6b684b0f54ba
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-08-04 21:14:44 +0545

    adding validation on userinformationadto

--- src/main/java/com/expense_tracker_app/expense_tracker_app/userinformation/dto/UserInformationDto.java
+++ src/main/java/com/expense_tracker_app/expense_tracker_app/userinformation/dto/UserInformationDto.java
@@ -1,5 +1,6 @@
 package com.expense_tracker_app.expense_tracker_app.userinformation.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -10,18 +11,21 @@ public class UserInformationDto {
 
     private Long id;
 
+    @NotBlank(message = "First name is required.")
     private String  firstName;
 
     private String middleName;
 
+    @NotBlank(message = "Middle name is required.")
     private String lastName;
 
     private Long age;
-
+    @NotBlank(message = "Date of birth is required.")
     private LocalDate dateOfBirth;
-
+    @NotBlank(message = "Username is required.")
     private String username;
 
+    @NotBlank(message = "Password is required.")
     private String password;
 
     private LocalDateTime createdAt;
