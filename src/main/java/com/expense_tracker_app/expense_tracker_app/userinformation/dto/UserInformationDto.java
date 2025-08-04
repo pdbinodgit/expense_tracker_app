65c25fac3dfde8c7360348cf5789fa68c81f9929
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-08-04 21:49:08 +0545

    adding userinformation controller

--- src/main/java/com/expense_tracker_app/expense_tracker_app/userinformation/dto/UserInformationDto.java
+++ src/main/java/com/expense_tracker_app/expense_tracker_app/userinformation/dto/UserInformationDto.java
@@ -2,11 +2,14 @@ package com.expense_tracker_app.expense_tracker_app.userinformation.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 public class UserInformationDto {
 
     private Long id;
