3bb52c7b5203184b626dd7a2ecd502757261adf1
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-08-06 22:21:24 +0545

    adding userservice class

---
+++ src/main/java/com/expense_tracker_app/expense_tracker_app/role/service/RoleService.java
@@ -0,0 +1,7 @@
+package com.expense_tracker_app.expense_tracker_app.role.service;
+
+import com.expense_tracker_app.expense_tracker_app.role.dto.RoleDto;
+
+public interface RoleService {
+    public RoleDto save(RoleDto roleDto);
+}
