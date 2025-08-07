3868b87c428e16f1cf346afbdb8b3ff6da590da8
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-08-07 20:53:07 +0545

    working on save role

--- src/main/java/com/expense_tracker_app/expense_tracker_app/role/controller/RoleController.java
+++ src/main/java/com/expense_tracker_app/expense_tracker_app/role/controller/RoleController.java
@@ -1,4 +1,24 @@
 package com.expense_tracker_app.expense_tracker_app.role.controller;
 
+import com.expense_tracker_app.expense_tracker_app.customresponse.ApiResponse;
+import com.expense_tracker_app.expense_tracker_app.role.dto.RoleDto;
+import com.expense_tracker_app.expense_tracker_app.role.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/role")
 public class RoleController {
+    @Autowired
+    RoleService roleService;
+
+    @PostMapping("/save")
+    public ResponseEntity<ApiResponse<?>> saveRole(@RequestBody RoleDto roleDto){
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(HttpStatus.OK,"Role save successfully",roleService.save(roleDto)));
+    }
 }
