ed946b6c16dba8829c9052d14709fdaa896d0a97
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-08-07 20:48:52 +0545

    working on find all role

--- src/main/java/com/expense_tracker_app/expense_tracker_app/role/service/RoleService.java
+++ src/main/java/com/expense_tracker_app/expense_tracker_app/role/service/RoleService.java
@@ -2,6 +2,9 @@ package com.expense_tracker_app.expense_tracker_app.role.service;
 
 import com.expense_tracker_app.expense_tracker_app.role.dto.RoleDto;
 
+import java.util.List;
+
 public interface RoleService {
     public RoleDto save(RoleDto roleDto);
+    public List<RoleDto> findAllRole(RoleDto roleDto);
 }
--- src/main/java/com/expense_tracker_app/expense_tracker_app/role/serviceImpl/RoleServiceImpl.java
+++ src/main/java/com/expense_tracker_app/expense_tracker_app/role/serviceImpl/RoleServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -27,5 +28,12 @@ public class RoleServiceImpl implements RoleService {
         }
         Role role=roleRepository.save(roleMapper.dtoToEntity(roleDto));
         return roleMapper.entityToDto(role);
+    }
+
+    @Override
+    public List<RoleDto> findAllRole(RoleDto roleDto) {
+        List<Role> roleList=roleRepository.findAll();
+        List<RoleDto> roleDtoList=roleList.stream().map(role -> roleMapper.entityToDto(role)).toList();
+        return roleDtoList;
     }
 }
