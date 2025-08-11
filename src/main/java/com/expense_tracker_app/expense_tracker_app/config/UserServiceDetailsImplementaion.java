71d74db840de742fc34be7fa12e3f3d2f9529489
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-08-11 22:26:16 +0545

    adding user principal class

---
+++ src/main/java/com/expense_tracker_app/expense_tracker_app/config/UserServiceDetailsImplementaion.java
@@ -0,0 +1,39 @@
+package com.expense_tracker_app.expense_tracker_app.config;
+
+import com.expense_tracker_app.expense_tracker_app.userinformation.model.UserInformation;
+import com.expense_tracker_app.expense_tracker_app.userinformation.repository.UserInformationRepository;
+import com.expense_tracker_app.expense_tracker_app.userinformation.service.UserInformationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+public class UserServiceDetailsImplementaion implements UserDetailsService {
+    /**
+     * Locates the user based on the username. In the actual implementation, the search
+     * may possibly be case sensitive, or case insensitive depending on how the
+     * implementation instance is configured. In this case, the <code>UserDetails</code>
+     * object that comes back may have a username that is of a different case than what
+     * was actually requested..
+     *
+     * @param username the username identifying the user whose data is required.
+     * @return a fully populated user record (never <code>null</code>)
+     * @throws UsernameNotFoundException if the user could not be found or the user has no
+     *                                   GrantedAuthority
+     */
+
+    @Autowired
+    UserInformationRepository userInformationRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserInformation> userInformationOptional=userInformationRepository.findByUsername(username);
+        if (!userInformationOptional.isPresent()){
+            throw new UsernameNotFoundException("User not found !!");
+        }
+
+        return UserPrincipal.build(userInformationOptional.get());
+    }
+}
