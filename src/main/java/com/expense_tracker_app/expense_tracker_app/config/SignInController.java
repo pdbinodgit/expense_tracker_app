0e400c47b6da2246c87c1fdaa2a632bcd7e585e6
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-08-12 20:32:55 +0545

    small change

--- src/main/java/com/expense_tracker_app/expense_tracker_app/config/SignInController.java
+++ src/main/java/com/expense_tracker_app/expense_tracker_app/config/SignInController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,7 @@ public class SignInController {
     @Autowired
     AuthenticationManager authenticationManager;
 
+    @PostMapping("/signIn")
     public ResponseEntity<ApiResponse<?>> signIn(@RequestBody TokenRequest tokenRequest){
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(tokenRequest.getUsername(), tokenRequest.getPassword()));
